2a40ee24757b37da82ebd7d23fda5bbd8bd939fd
Mickaël Véril <mika.veril@wanadoo.fr>
2025-02-01 17:02:41 +0100

    Fix formating

--- wslplugins-rs/src/api/errors/require_update_error.rs
+++ wslplugins-rs/src/api/errors/require_update_error.rs
@@ -4,10 +4,10 @@
 //! does not meet the required version. It integrates with Windows error codes for seamless interop
 //! with WSL APIs.
 
-use crate::{WSLVersion};
-use wslpluginapi_sys::WSL_E_PLUGIN_REQUIRES_UPDATE;
+use crate::WSLVersion;
 use thiserror::Error;
 use windows::core::HRESULT;
+use wslpluginapi_sys::WSL_E_PLUGIN_REQUIRES_UPDATE;
 
 /// Represents an error when the current WSL version is unsupported.
 ///
